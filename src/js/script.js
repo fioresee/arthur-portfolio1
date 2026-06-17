2a30c21b0369de03f8f57e6dfd75601a80ea44cb
Arthur Fiorese <arthur.herrada@gmail.com>
2026-06-17 11:55:17 -0300

    feat: adição da seção 'projetos' com carrossel e slideshow de fotos

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// --- Animação do Header ---------------------------
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
@@ -9,3 +11,48 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+// --- Carrossel de Projetos -------------------------
+
+(function () {
+  const slides = document.querySelectorAll('.slide');
+  const dots   = document.querySelectorAll('.dot');
+  let atual    = 0;
+  let timer;
+
+  function irPara(index) {
+    slides[atual].classList.remove('ativo');
+    dots[atual].classList.remove('ativo');
+    atual = (index + slides.length) % slides.length;
+    slides[atual].classList.add('ativo');
+    dots[atual].classList.add('ativo');
+  }
+
+  function iniciarTimer() {
+    timer = setInterval(() => irPara(atual + 1), 4000);
+  }
+
+  function reiniciarTimer() {
+    clearInterval(timer);
+    iniciarTimer();
+  }
+
+  document.getElementById('btn-proximo').addEventListener('click', () => {
+    irPara(atual + 1);
+    reiniciarTimer();
+  });
+
+  document.getElementById('btn-anterior').addEventListener('click', () => {
+    irPara(atual - 1);
+    reiniciarTimer();
+  });
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+      irPara(Number(dot.dataset.index));
+      reiniciarTimer();
+    });
+  });
+
+  iniciarTimer();
+})();
